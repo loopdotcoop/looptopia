eba5a5d5ec291ddca20e1095ad6cae8413e11d7d
richplastow <rich.plastow@kandimedia.com>
2014-08-30 22:04:22 +0100

    + looptopia@0.1.9-2      ‘account.register’ form is functional and styled;

--- about/about.config.js
+++ about/about.config.js
@@ -8,7 +8,7 @@ Config = {
         name:         'Looptopia'
       , slug:         'looptopia' // equivalent to the `'name'` field in ‘package.json’ 
       , initials:     'lta'
-      , version:      '0.1.9-1'
+      , version:      '0.1.9-2'
       , description:  'A cube-shaped planet in the Loop.Coop system.' // no more than 255 characters
       , keywords:     'Brighton, art, music, immersive, app, game, cooperative, party, festival'
       , scripts: {
@@ -65,6 +65,7 @@ Config = {
           , '+ looptopia@0.1.8-fix.4  don’t show ‘About This Project’ on 404 page; '
           , '+ looptopia@0.1.8        ok for live server, up until 1st Sept; '
           , '+ looptopia@0.1.9-1      BabelSlug for username; need to prevent user records from sharing a username; '
+          , '+ looptopia@0.1.9-2      ‘account.register’ form is functional and styled; '
 
         ]
     }
