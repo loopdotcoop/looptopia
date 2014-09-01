e7015c0dfd0ad7cf9045866b7c7184e591bccfb7
richplastow <rich.plastow@kandimedia.com>
2014-09-01 21:17:59 +0100

    + layout@0.0.4      Loop.Coop logo at top of every page; hide full twitter-id on mobile;

--- layout/layout.config.js
+++ layout/layout.config.js
@@ -18,10 +18,11 @@ Config.layout = {
         '+ layout@0.0.1-1    create ‘layout’ as a feature of ‘looptopia@0.1.3-5’; '
       , '+ layout@0.0.2      mock-merge into develop/looptopia@0.1.3-9; '
       , '+ layout@0.0.3-1    `#iframe` fills screen; `#iframe-blocker.active` dims iframe and catches mouse events; '
-      , '+ layout@0.0.3-2    `#iframe-blocker` active on all but the home page;  \n'
+      , '+ layout@0.0.3-2    `#iframe-blocker` active on all but the home page;  \n' +
         '                    dismiss a popup (actually a route) by clicking anywhere on the background; '
+      , '+ layout@0.0.4      Loop.Coop logo at top of every page; hide full twitter-id on mobile; '
     ]
-  , version:     '0.0.3-2'
+  , version:     '0.0.4'
 };
 
 Router.configure({
