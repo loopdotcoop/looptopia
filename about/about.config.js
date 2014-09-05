1e7d43712f7a3f71d1cdf4fe2aa8f9e00097af86
richplastow <rich.plastow@kandimedia.com>
2014-09-05 21:47:44 +0100

    + looptopia@0.2.7-1      serverside `Config.about` has `env` field, which equals `NODE_ENV` at startup;

--- about/about.config.js
+++ about/about.config.js
@@ -86,12 +86,18 @@ Config = {
           , '+ looptopia@0.2.3-2      all ‘popups’ (which are actually pages) have a ‘dismiss’ button; '
           , '+ looptopia@0.2.4        various UI tweaks and renamed features; '
           , '+ looptopia@0.2.6        redone release; '
+          , '+ looptopia@0.2.7-1      serverside `Config.about` has `env` field, which equals `NODE_ENV` at startup; '
         ]
-      , version:        '0.2.6'
+      , version:        '0.2.7-1'
     }
 }
+
+
+if ('object' === typeof process) { Config.about.env = process.env.NODE_ENV; } // 'development' on localhost, 'production' on modulus
+
 
 Router.configure({
     notFoundTemplate: 'about.not-found' // catchall 404 https://github.com/EventedMind/iron-router#route-options
 });
 
+console.log(Config.about.env);
